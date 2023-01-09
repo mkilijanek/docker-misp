194bf8fc98cfd411bf3f76f475cb2fe8dd9f576a
Kili <michal@kilijanek.pl>
2023-01-09 17:50:45 +0100

    moving to submodules

--- server/files/entrypoint-workers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-while true; do
-    echo "[workers] Starting"
-    sudo -u www-data -g www-data /var/www/MISP/app/Console/worker/start.sh
-    echo "[workers] Started"
-
-    sleep 3600
-done
